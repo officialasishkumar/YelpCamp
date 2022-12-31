b8bb6cb6c7e529d991292298817352901be4b4c6
Asish Kumar <officialasishkumar@gmail.com>
2022-12-31 18:36:59 +0530

    updated siteID

--- app.js
+++ app.js
@@ -154,7 +154,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 2000;
 app.listen(port, () => {
     console.log(`serving on port ${port}`)
 })
